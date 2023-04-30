5a3d68b9d9e55fd3dad725c6777cb1b696ec8523
Oliver Fleckenstein <oliverfl@live.dk>
2023-04-30 15:57:21 +0100

    build: Environment variables and updated script to load data

--- scripts/start_db.sh
+++ scripts/start_db.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env sh
 
+docker stop budget-db && docker rm budget-db
+
 docker run \
     -p 5432:5432 \
+    -e POSTGRES_USER=budget_user \
     -e POSTGRES_PASSWORD=password \
     -d \
     --name budget-db \
     postgres
+
+# Sleep for a second to ensure postgres has stated up.
+sleep 1
+
+if [ ! -z ${1+x} ]; then
+    echo "Importing from $1"
+    cat $1 | docker exec -i budget-db psql -U budget_user
+fi
+
